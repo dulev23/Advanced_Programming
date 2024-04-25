a29b9d7c9ea4132dd355f789aed4a8e5917ac2df
Nikola Dulev <nikoladulev13@gmail.com>
2024-04-26 00:23:20 +0200

    Create VtorKolokvium

---
+++ src/VtorKolokvium/BooksTest.java
@@ -0,0 +1,98 @@
+package VtorKolokvium;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Book {
+    String title;
+    String category;
+    float price;
+
+    public Book(String title, String category, float price) {
+        this.title = title;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%s) %.2f",title,category,price);
+    }
+    public float getPrice() {
+        return price;
+    }
+}
+
+class BookCollection {
+    List<Book> books;
+
+    public BookCollection() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+    public void printByCategory(String category){
+        Comparator<Book> comparator = Comparator.comparing(Book::getTitle).thenComparing(book -> book.price);
+        books.stream()
+                .filter(book -> book.getCategory().equalsIgnoreCase(category))
+                .sorted(comparator)
+                .forEach(System.out::println);
+    }
+
+    public List<Book> getCheapestN(int n) {
+        Comparator<Book> comparator = Comparator.comparing(Book::getPrice).thenComparing(Book::getTitle);
+        return books.stream()
+                .sorted(comparator)
+                .limit(n)
+                .collect(Collectors.toList());
+    }
+}
+
+public class BooksTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        BookCollection booksCollection = new BookCollection();
+        Set<String> categories = fillCollection(scanner, booksCollection);
+        System.out.println("=== PRINT BY CATEGORY ===");
+        for (String category : categories) {
+            System.out.println("CATEGORY: " + category);
+            booksCollection.printByCategory(category);
+        }
+        System.out.println("=== TOP N BY PRICE ===");
+        print(booksCollection.getCheapestN(n));
+    }
+
+    static void print(List<Book> books) {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    static TreeSet<String> fillCollection(Scanner scanner,
+                                          BookCollection collection) {
+        TreeSet<String> categories = new TreeSet<String>();
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(":");
+            Book book = new Book(parts[0], parts[1], Float.parseFloat(parts[2]));
+            collection.addBook(book);
+            categories.add(parts[1]);
+        }
+        return categories;
+    }
+}
+
+// Вашиот код овде
